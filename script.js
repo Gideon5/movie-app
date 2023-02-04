8bc1c2846b27e52216ec7bd11ff0f2d3e3a8c732
elgrande <abaaneg@gmail.com>
2023-02-04 13:33:42 +0000

    wip

--- script.js
+++ script.js
@@ -3,3 +3,60 @@ const APIURL =
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+    const main = document.querySelector('main')
+    const form = document.querySelector('form')
+    const search = document.getElementById('search')
+
+    //Initially get fav movies
+    getMovies()
+
+    async function getMovies() {
+        const resp = await fetch(APIURL)
+        const respData = await resp.json()
+
+        console.log(respData)
+
+        respData.results.forEach((e) => {
+            const { poster_path, title, vote_average} = e
+            const movieEl = document.createElement('div')
+            movieEl.classList.add("movie")
+
+            movieEl.innerHTML = `
+
+                <img 
+                src="${IMGPATH + poster_path}" 
+                alt="${title}"
+                /> 
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>    
+            
+            `
+
+            document.body.appendChild(movieEl)
+        })
+
+        return respData
+    }
+
+function getClassByRate(vote){
+    if(vote > 0){
+        return 'green'
+    }
+    else if (vote >=5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+})
+
